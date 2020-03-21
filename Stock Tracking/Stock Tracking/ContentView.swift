4ed6d9a43c3f30fc9d475fa9b6b8c4594f309efb
leoMehlig <leo@leomehlig.com>
2020-03-21 11:17:02 +0100

    List

--- Stock Tracking/Stock Tracking/ContentView.swift
+++ Stock Tracking/Stock Tracking/ContentView.swift
@@ -1,11 +1,3 @@
-//
-//  ContentView.swift
-//  Stock Tracking
-//
-//  Created by Leo Mehlig on 21.03.20.
-//  Copyright © 2020 wIrvsvirus. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
