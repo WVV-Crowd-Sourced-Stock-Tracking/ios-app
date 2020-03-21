a68ebf4704e5d5918958545cf5062392716ae0ce
Veit Progl <mail@veit.dev>
2020-03-21 11:19:09 +0100

    shopingList

--- Stock Tracking/Stock Tracking/View/ShopingList.swift
+++ Stock Tracking/Stock Tracking/View/ShopingList.swift
@@ -9,28 +9,16 @@
 import SwiftUI
 
 struct ShopingList: View {
-	@State var landmarks: [Landmark] = [
-		   Landmark(name: "Sydney Harbour Bridge", location: .init(latitude: -33.852222, longitude: 151.210556)),
-		   Landmark(name: "Brooklyn Bridge", location: .init(latitude: 40.706, longitude: -73.997)),
-		   Landmark(name: "Golden Gate Bridge", location: .init(latitude: 37.819722, longitude: -122.478611))
-	   ]
-	@State var selectedLandmark: Landmark? = nil
-	
     var body: some View {
 		NavigationView() {
 			List() {
-				Section(header: Text("Store")) {
-					HStack() {
-						MapView(landmarks: $landmarks, selectedLandmark: $selectedLandmark)
-							.frame(width: 200, height: 200)
-						VStack() {
-							Text("Shop Name").font(Font.headline).bold()
-						}
+				HStack() {
+					VStack() {
+						Text("Kategorie")
 					}
 				}
 			}
 			.navigationBarTitle("Einkaufsliste")
-			.listStyle(GroupedListStyle())
 		}
     }
 }
